634e8d23640326c7e8f21482fe68d6c8ae55f331
Durga-Pavan-Sriram-Sannidhi <165423368+Sannidhi-Sriram1@users.noreply.github.com>
2025-03-27 22:03:50 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-def hash_password(password):
-    """Hash a password using bcrypt."""
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-
-def verify_password(password, hashed_password):
-    """Verify a password against a stored bcrypt hash."""
-    return bcrypt.checkpw(password.encode(), hashed_password.encode())
